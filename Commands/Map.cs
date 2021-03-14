194a7700d7a29d4ec4dcfa326ea60bb897d9840c
M3IY0U <timo.nock@stud.h-da.de>
2021-03-14 21:53:31 +0100

    Proof of concept for map rendering/preview

--- Commands/Map.cs
+++ Commands/Map.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
@@ -10,6 +11,7 @@ using DSharpPlus.Interactivity.Extensions;
 using Newtonsoft.Json;
 using QuaverBot.Core;
 using QuaverBot.Entities;
+using QuaverBot.Graphics;
 
 namespace QuaverBot.Commands
 {
@@ -19,7 +21,7 @@ namespace QuaverBot.Commands
         public Map(Config config) => _config = config;
 
         [Command("mapsearch"), Aliases("ms", "cs")]
-        public async Task MapGroupCommand(CommandContext ctx, [RemainingText] string search = "")
+        public async Task MapSearch(CommandContext ctx, [RemainingText] string search = "")
         {
             var pages = await SearchForMapset(search, ctx);
             // don't waste resources paginating a message that only has 1 page anyway
@@ -34,6 +36,15 @@ namespace QuaverBot.Commands
             }
         }
 
+        [Command("mappreview"), Aliases("qmp")]
+        public async Task MapPreviewCommand(CommandContext ctx, string id, int begin, int length)
+        {
+            new WebClient().DownloadFile($"https://api.quavergame.com/d/web/map/{id}", $"{id}.qua");
+            await ctx.Message.CreateReactionAsync(DiscordEmoji.FromUnicode("♨"));
+            MapPreview.RenderMap($"{id}.qua", Convert.ToInt32(id), begin, length);
+            await ctx.RespondAsync(new DiscordMessageBuilder().WithFile($"{id}.mp4"));
+        }
+        
         [Command("map"), Aliases("m", "c")]
         public async Task MapInfo(CommandContext ctx, string input = "")
         {
